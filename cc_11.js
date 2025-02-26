055c0ea33b3316fcb5282f4df740eeac63cb5ac3
Lukeguy334 <lhs57@usf.edu>
2025-02-26 14:09:57 -0500

    Task 1 - Created Book Class

---
+++ cc_11.js
@@ -0,0 +1,26 @@
+// Task 1 - Creating a book class
+class Book {
+    constructor(title, author, isbn, copies) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.copies = copies;
+    }
+    getDetails() {
+        return `${this.title} by ${this.author}, ISBN: ${this.isbn}, Copies: ${this.copies}`;
+    }
+
+    updateCopies(quantity) {
+        this.copies += quantity;
+    }
+}
+
+// Test Cases
+const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
+
+book1.updateCopies(-1);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
